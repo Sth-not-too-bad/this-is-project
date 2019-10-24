ca6de096dbce4af70ded260e83a89bd0400f462d
tiandahui <52695246+tiandahui@users.noreply.github.com>
2019-10-24 21:51:51 +0800

    user-1.0

--- mzcars/src/router.js
+++ mzcars/src/router.js
@@ -1,9 +1,11 @@
 import React,{Component} from 'react'
 import ComponentImport from './utils/componentImport'
 import {HashRouter,Switch,Redirect,Route} from 'react-router-dom'
-
 import Cars from  './cars'
 
+
+const User = ComponentImport(()=>import('./component/user'))
+const AddUser = ComponentImport(()=>import('./component/user/AddUser'))
 const Eg = ComponentImport(()=>import('./component/eg'))
 const Eg1 = ComponentImport(()=>import('./component/eg1'))
 
@@ -19,7 +21,8 @@ class RootRouter extends Component{
                     return(
                         <Cars>
                             <Route path='/cars/home' component={Eg}></Route>
-                            <Route path='/cars/customers' component={Eg1}></Route>
+                            <Route path='/cars/customers' component={User}></Route>
+                            <Route path='/cars/coustom/adduser' component={AddUser}></Route>
                         </Cars>
                     )
                 }}></Route>
