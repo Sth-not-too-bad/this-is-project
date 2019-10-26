eca56223ddc9a572c7fe0512f08df7c54304cf43
zlymyself <52694180+zlymyself@users.noreply.github.com>
2019-10-26 12:33:05 +0800

    home

--- mzcars/src/router.js
+++ mzcars/src/router.js
@@ -4,8 +4,10 @@ import { HashRouter, Switch, Redirect, Route } from 'react-router-dom'
 import Cars from './cars'
 import { Provider } from 'react-redux';
 import store from './store';
+import home from './component/roles/home';
 
 const Login = ComponentImport(()=>import("./component/login"))
+const Home = ComponentImport(()=>import("./component/home"))
 const User = ComponentImport(() => import('./component/user'))
 const AddUser = ComponentImport(() => import('./component/user/AddUser'))
 const Roles = ComponentImport(() => import('./component/roles'))
@@ -24,6 +26,7 @@ class RootRouter extends Component {
                         <Route path='/cars' render={() => {
                             return (
                                 <Cars>
+                                    <Route path='/cars' component={Home}></Route>
                                     <Route path='/cars/customers' component={User}></Route>
                                     <Route exact path='/cars/set/roles' component={Roles}></Route>
                                     <Route path='/cars/coustom/adduser' component={AddUser}></Route>
